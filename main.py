aa69968464802e5f5b0560fe9e5cab536ad80227
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-06-25 14:10:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import ttbotapi
 import random
 
+PICS = [
+ "file:///C:/Users/user/Downloads/343618100_939688137480555_5228557919266828998_n.jpg.html"
+]
+
 bot = ttbotapi.Bot(access_token='wko_5FDZB0gOIGBaKizYBYdCIAX2sz4mIR2nxnmOrXo')
 
 @bot.update_handler(chat_type='dialog', bot_command=['start'])
