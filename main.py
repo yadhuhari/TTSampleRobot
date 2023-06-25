49e73b2d3dd64b288c8246c793ebe13382fe3e72
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-06-25 14:43:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ PICS = [
  "file:///C:/Users/user/Downloads/343618100_939688137480555_5228557919266828998_n.jpg.html"
 ]
 
-Client = ttbotapi.Bot(access_token='wko_5FDZB0gOIGBaKizYBYdCIAX2sz4mIR2nxnmOrXo')
+bot = ttbotapi.Bot(access_token='wko_5FDZB0gOIGBaKizYBYdCIAX2sz4mIR2nxnmOrXo')
 
-@Client.update_handler(chat_type='dialog', bot_command=['start'])
+@bot.update_handler(chat_type='dialog', bot_command=['start'])
 def start (update):
   bot.send_photo(
     photo=random.choice(PICS),
@@ -19,4 +19,4 @@ How Are You..!!"""
 
 print("Bot Started..!!")
 
-Client.run()
+bot.polling()
